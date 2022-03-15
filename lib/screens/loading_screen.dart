dc4ede6733bc27c86010208848bda9c98a91660e
황등종로약국 <flypeace@naver.com>
2022-03-15 17:21:48 +0900

    약국에서

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -9,7 +9,7 @@ class LoadingScreen extends StatefulWidget {
 class _LoadingScreenState extends State<LoadingScreen> {
 
   void getLocation() async {
-    Position position = await Geolocator().ge tCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
     print(position);
   }
 
