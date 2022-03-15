9abffe38415852e8b84610544c23f82768709b22
yongchulbag <yongchulbag@gmail.com>
2022-03-16 00:01:39 +0900

    집에서

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location_get.dart';
 import 'package:clima/services/network_get_data.dart';
+import 'location_weather_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 
 const apiKey = 'cadf49837ccb3c2815996601f83586f9';
@@ -25,18 +27,30 @@ class _LoadingScreenState extends State<LoadingScreen> {
     Location user_location = Location();
     await user_location.getCurrentLocation();
 
-    latitude=user_location.latitude;
-    longitude=user_location.longitude;
+    latitude = user_location.latitude;
+    longitude = user_location.longitude;
 
-    NetworkGetData networkGetData = NetworkGetData('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude@appid=$apiKey');
+    NetworkGetData networkGetData = NetworkGetData(
+        'https://api.openweathermap.org/data/2.5/onecall?lat=$latitude&lon=$longitude&exclude=hourly,daily&appid=$apiKey');
 
     var received_weatherData = await networkGetData.get();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+    return LocationScreen();
+    }));
 
   }
 
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitFadingCube(
+          color: Colors.white,
+          size: 150,
+        ),
+      )
+    );
   }
 }
